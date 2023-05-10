e07e018d86d696ec3971a9114fd7e6609b84453c
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-10 14:18:22 -0700

    created initial uploaditem function

--- backend/src/item.js
+++ backend/src/item.js
@@ -1,3 +1,27 @@
-exports.uploadItem = (req, res) => {
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+exports.uploadItem = async (req, res) => {
+  const body = req.body;
+
+  const insert = 'Insert into item VALUES ($1, $2)';
+  const insertQuery = {
+    text: insert,
+    values: [req.user.id, body],
+  };
+
+  const {rows} = await pool.query(insertQuery);
+  if (rows.length != 1) {
+    return res.status(400).json('Bad request');
+  }
+
+  return res.status(201).json('Item has been created');
   console.log('upload item');
 };
