4cf78060eb5bab189963ee8eff0cc4da7c139f5f
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-11 17:49:57 -0700

    added create item sidebar

---
+++ frontend/src/components/CreateItem.jsx
@@ -0,0 +1,12 @@
+import {Button} from '@mui/material';
+import React from 'react';
+
+const CreateItem = () => {
+  return (
+    <div>
+      <Button>Create Items</Button>
+    </div>
+  );
+};
+
+export default CreateItem;
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,11 +1,44 @@
+
+
 import React from 'react';
 import Items from './Items';
+import CreateItem from './CreateItem';
+
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {Box, Container, Grid} from '@mui/material';
+
+
+const mdTheme = createTheme();
 
 const Dashboard = () => {
   return (
     <div>
       Hello from dashboard
-      <Items/>
+      <ThemeProvider theme={mdTheme}>
+        <Box sx={{display: 'flex'}}>
+          <CreateItem />
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'? theme.palette.grey[100] :
+                  theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+            <Container maxWidth="lg" sx={{mt: 4, mb: 4}}>
+              <Grid container spacing={3}>
+                {/* Recent Orders */}
+                <Grid item xs={12}>
+                  <Items />
+                </Grid>
+              </Grid>
+            </Container>
+          </Box>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 };
