99a7669accdd631e44bbfaba6c3b12eb9fb24718
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-12 19:08:37 -0700

    modified items styling

--- frontend/src/components/Items.jsx
+++ frontend/src/components/Items.jsx
@@ -2,16 +2,15 @@ import React, {useEffect, useState} from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
+// import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import {red} from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Grid} from '@mui/material';
 
 import './../css/App.css';
@@ -72,30 +71,11 @@ const Items = () => {
             items.map((row) => (
               <Grid item key={row.itemid} xs={12} sm={6} md={4}>
                 <Card>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{bgcolor: red[500]}} aria-label="recipe">R
-                      </Avatar>
-                    }
-                    action={
-                      <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                      </IconButton>
-                    }
-                    title={row.data.name}
-                    subheader="September 14, 2016"
-                  />
                   <CardMedia
                     component="img"
                     src={row.data.imageUrl}
-                    alt="Paella dish"
+                    alt="Product Image"
                   />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          if you like.
-                    </Typography>
-                  </CardContent>
                   <CardActions disableSpacing>
                     <IconButton aria-label="add to favorites">
                       <FavoriteIcon />
@@ -104,27 +84,13 @@ const Items = () => {
                       <ShareIcon />
                     </IconButton>
                   </CardActions>
-                  <Collapse timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <Typography paragraph>Method:</Typography>
-                      <Typography paragraph>
-            aside for 10 minutes.
-                      </Typography>
-                      <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep sk
-                      </Typography>
-                      <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-                      </Typography>
-                      <Typography>
-            Set aside off of the heat to let
-                      </Typography>
-                    </CardContent>
-                  </Collapse>
+                  <CardHeader
+                    title={row.data.product}
+                    avatar={
+                      <Avatar sx={{bgcolor: red[500]}} aria-label="recipe">R
+                      </Avatar>
+                    }
+                  />
                 </Card>
               </Grid>
 
