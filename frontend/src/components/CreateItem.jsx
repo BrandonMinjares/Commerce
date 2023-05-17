ce6a8c015ab1f2ee234aa11ec73ced9c3b80479c
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-17 16:37:23 -0700

    fixed stlying on create product page

--- frontend/src/components/CreateItem.jsx
+++ frontend/src/components/CreateItem.jsx
@@ -1,7 +1,17 @@
 import {Box, Button, FormControl, Grid, MenuItem,
-  TextField, TextareaAutosize} from '@mui/material';
+  TextField,
+  Typography} from '@mui/material';
 import React, {useState} from 'react';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
 
 /**
  * @return {void}
@@ -47,137 +57,150 @@ export default function CreateItem() {
   };
 
   return (
-    <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 1}}>
-      <FormControl>
-        <Grid container spacing={2} sx={{width: '75%'}}>
-          <Grid item sm={12}>
-            <TextField
-              type="file"
-              inputProps={{accept: 'image/*'}}
-              name='file'
-              id="file"
-              fullWidth
-            />
-          </Grid>
-          <Grid item sm={6}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="product"
-              label="Product Name"
-              name="product"
-              placeholder="Enter Name"
-              autoFocus
-              aria-label='Product Name'
-            />
-            <TextField
-              select
-              margin="normal"
-              required
-              fullWidth
-              id="category"
-              label="Category"
-              name="category"
-              value={category}
-              onChange={handleCategoryChange}
-              placeholder="Select category"
-              autoFocus
-              aria-label='Category'
-            >
-              <MenuItem value={'Vehicles'}>Vehicles</MenuItem>
-              <MenuItem value={'Property'}>Property</MenuItem>
-              <MenuItem value={'Apparel'}>Apparel</MenuItem>
-              <MenuItem value={'Electronics'}>Electronics</MenuItem>
-              <MenuItem value={'Computers'}>Computers</MenuItem>
-              <MenuItem value={'Instruments'}>Instruments</MenuItem>
-              <MenuItem value={'Toys & Games'}>Toys & Games</MenuItem>
-              <MenuItem value={'Sporting Goods'}>Sporting Goods</MenuItem>
-              <MenuItem value={'Home Goods'}>Home Goods</MenuItem>
-            </TextField>
+    <ThemeProvider theme={theme}>
 
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="price"
-              label="$ Price"
-              name="price"
-              autoFocus
-              aria-label='Price'
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="quantity"
-              label="Quantity"
-              name="quantity"
-              autoFocus
-              aria-label='Quantity'
-              min={0}
+      <Box component="form" noValidate onSubmit={handleSubmit}
+        sx={{mt: 2, justifyContent: 'center'}}>
+        <Typography textAlign={'center'} fontSize={30}>
+          Create a Product
+        </Typography>
+        <Grid container direction='row' justifyContent="center">
+          <FormControl>
+            <Grid container spacing={2}>
+              <Grid item sm={12}>
+                <TextField
+                  type="file"
+                  inputProps={{accept: 'image/*'}}
+                  name='file'
+                  id="file"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="product"
+                  label="Product Name"
+                  name="product"
+                  placeholder="Enter Name"
+                  autoFocus
+                  aria-label='Product Name'
+                />
+                <TextField
+                  select
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="category"
+                  label="Category"
+                  name="category"
+                  value={category}
+                  onChange={handleCategoryChange}
+                  placeholder="Select category"
+                  autoFocus
+                  aria-label='Category'
+                >
+                  <MenuItem value={'Vehicles'}>Vehicles</MenuItem>
+                  <MenuItem value={'Property'}>Property</MenuItem>
+                  <MenuItem value={'Apparel'}>Apparel</MenuItem>
+                  <MenuItem value={'Electronics'}>Electronics</MenuItem>
+                  <MenuItem value={'Computers'}>Computers</MenuItem>
+                  <MenuItem value={'Instruments'}>Instruments</MenuItem>
+                  <MenuItem value={'Toys & Games'}>Toys & Games</MenuItem>
+                  <MenuItem value={'Sporting Goods'}>Sporting Goods</MenuItem>
+                  <MenuItem value={'Home Goods'}>Home Goods</MenuItem>
+                </TextField>
 
-            />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="price"
+                  label="$ Price"
+                  name="price"
+                  autoFocus
+                  aria-label='Price'
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="quantity"
+                  label="Quantity"
+                  name="quantity"
+                  autoFocus
+                  aria-label='Quantity'
+                  min={0}
 
-            <TextField
-              select
-              margin="normal"
-              required
-              fullWidth
-              id="condition"
-              label="Condition"
-              name="condition"
-              value={condition}
-              onChange={handleConditionChange}
-              placeholder="Select condition"
-              autoFocus
-              aria-label='Condition'
-            >
-              <MenuItem value={'Like New'}>Like New</MenuItem>
-              <MenuItem value={'Very Good'}>Very Good</MenuItem>
-              <MenuItem value={'Good'}>Good</MenuItem>
-              <MenuItem value={'Acceptable'}>Acceptable</MenuItem>
-            </TextField>
-          </Grid>
+                />
 
-          <Grid item sm={6}>
-            <TextareaAutosize
-              aria-label="Description"
-              minRows={20}
-              margin="auto"
-              label="Description"
-              placeholder="Enter Product Description"
-              name='description'
-              id="description"
-            />
-          </Grid>
-          <Grid item sm={12}>
-            <Grid item sm={6}>
-              <Button
-                type="submit"
-                aria-label='Cancel'
-                fullWidth
-                variant="contained"
-                sx={{mt: 3, mb: 2}}
-              >
+                <TextField
+                  select
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="condition"
+                  label="Condition"
+                  name="condition"
+                  value={condition}
+                  onChange={handleConditionChange}
+                  placeholder="Select condition"
+                  autoFocus
+                  aria-label='Condition'
+                >
+                  <MenuItem value={'Like New'}>Like New</MenuItem>
+                  <MenuItem value={'Very Good'}>Very Good</MenuItem>
+                  <MenuItem value={'Good'}>Good</MenuItem>
+                  <MenuItem value={'Acceptable'}>Acceptable</MenuItem>
+                </TextField>
+              </Grid>
+
+              <Grid item sm={6}>
+                <TextField
+                  margin="normal"
+                  required
+                  aria-label="Description"
+                  fullWidth
+                  multiline
+                  label="Description"
+                  placeholder="Enter Product Description"
+                  name='description'
+                  id="description"
+                  InputProps={{
+                    rows: 6,
+                  }}
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <Button
+                  color='neutral'
+                  type="submit"
+                  aria-label='Cancel'
+                  fullWidth
+                  variant="contained"
+                  sx={{mt: 3, mb: 2}}
+                >
               Cancel
-              </Button>
-            </Grid>
-            <Grid item sm={6}>
-              <Button
-                type="submit"
-                aria-label='Create'
-                fullWidth
-                variant="contained"
-                sx={{mt: 3, mb: 2}}
-              >
+                </Button>
+              </Grid>
+              <Grid item sm={6} justifyContent={'center'}>
+                <Button
+                  color='primary'
+                  type="submit"
+                  aria-label='Create'
+                  fullWidth
+                  variant="contained"
+                  sx={{mt: 3, mb: 2}}
+                >
               Create
-              </Button>
+                </Button>
+              </Grid>
             </Grid>
-
-          </Grid>
+          </FormControl>
         </Grid>
-      </FormControl>
-    </Box>
+      </Box>
+    </ThemeProvider>
   );
 };
