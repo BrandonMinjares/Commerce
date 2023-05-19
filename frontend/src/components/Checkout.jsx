ffea50cc859f343830fcabd9fcf93f5bd3c531f9
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-19 15:50:06 -0700

    modified shopping cart styling

--- frontend/src/components/Checkout.jsx
+++ frontend/src/components/Checkout.jsx
@@ -90,31 +90,29 @@ export default function Checkout() {
     <div>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={12} lg={12}>
-          <Typography textAlign={'center'} fontSize={28}>
+          <Typography textAlign={'center'} padding={2}
+            fontSize={38}>
             Shopping Cart
           </Typography>
         </Grid>
 
         <Grid item xs={8} sm={8} md={8} lg={8}>
           {items.length > 0 &&
-            items.map((row, index) => (
+            items.map((row) => (
               <Grid item key={row.itemid} xs={12} sm={12} md={12}>
                 <Card sx={{display: 'flex', padding: 2}}>
                   <CardMedia
                     component="img"
                     alt="Card Image"
                     src={row.data.urlLink}
-                    style={{width: 200, height: 'auto'}}
+                    style={{width: 300, height: 'auto'}}
                   />
                   <Box sx={{display: 'flex', flexDirection: 'column'}}>
                     <CardContent>
-                      <Typography variant="h5" component="h2">
+                      <Typography variant="h4" component="h2">
                         {row.data.product}
                       </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        {row.data.description}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
+                      <Typography variant="h5" color="textSecondary">
                         ${row.data.price}
                       </Typography>
                     </CardContent>
@@ -124,8 +122,10 @@ export default function Checkout() {
             ))}
         </Grid>
         <Grid item xs={4} sm={4} md={4} lg={4}>
-            Cost
-          <Button onClick={() => checkout()}>Checkout</Button>
+          <Button onClick={() => checkout()}>
+            <Typography
+              textAlign={'center'} fontSize={30}>Checkout</Typography>
+          </Button>
         </Grid>
       </Grid>
 
